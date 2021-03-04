0d0aa662796a5702ddc3ede74279a4f19fa69028
Jakson Ewell <76876544+Jrewel@users.noreply.github.com>
2021-03-03 20:31:52 -0700

    Add files via upload

---
+++ quick-stuff/AI.js
@@ -0,0 +1,10 @@
+var greating = "Hello, ";
+var name = prompt("What is your name?");
+alert(greating + name + "!");
+
+var country = prompt("What country are you from?")
+alert("That's cool! I've always wanted to visit " + country);
+
+prompt(name + ", how many licks does it take to get to the tootsie pop center of a tootsie pop?")
+alert("Let's find out, shall we?")
+alert("I counted 374 licks... Hooray!!")
