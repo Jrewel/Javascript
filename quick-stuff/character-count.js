e40d2cb89f6c0c601ebc871e811ad6b94e5b3b10
Jakson Ewell <76876544+Jrewel@users.noreply.github.com>
2021-03-03 20:32:15 -0700

    Add files via upload

---
+++ quick-stuff/character-count.js
@@ -0,0 +1,8 @@
+alert(prompt("Type Tweet Here").slice(0,140));
+
+//Programmers count from ZERO, first letter is in position ZERO
+
+var count = text_box.length;
+
+alert("You have written " + count + " characters, you have " + (140 - count) + " remaining.")
+
