40a2b960e6127d49c97d14010a0dfc54850bbb34
Jakson Ewell <76876544+Jrewel@users.noreply.github.com>
2021-03-07 10:14:32 -0700

    Add files via upload

---
+++ quick-stuff/dog-name-calculator.js
@@ -0,0 +1,4 @@
+var dogAge = prompt("Want to know how old your dog is in human years? Type your pups age below and we'll calculate it for you!");
+var humanAge = ((dogAge - 2) * 4) + 21; 
+
+alert("Wow! Your dog is " + humanAge + " years old in human years!");
